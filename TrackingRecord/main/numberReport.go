953fa2e26e7fd7ca2fc9333e9885bfeaf24dbfda
Shravan Venkataraman <heraclesphaeton@gmail.com>
2015-07-28 19:04:20 +0530

    updated NumberReport structure with relevant functions

--- TrackingRecord/main/numberReport.go
+++ TrackingRecord/main/numberReport.go
@@ -1,7 +1,10 @@
 package main
 
 import "container/list"
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 type TrackingNumberReport struct {
 	caseName string
@@ -19,4 +22,39 @@ func (obj TrackingNumberReport) returnReport() *list.List {
 	sort.Sort(finalList)
 	finalList.PushFront(caseName)
 	return finalList
+}
+
+func (obj TrackingNumberReport) generateTuple(tuple string) {
+
+	tupleArray := strings.Split(tuple, " ")
+	low := tupleArray[0]
+	high := tupleArray[1]
+	statusCode := tupleArray[2]
+	transferCode := tupleArray[3]
+
+	tN = TrackingNumber {low, high, statusCode, transferCode}
+	checkRangeAndStatus(tN)
+}
+
+func (obj TrackingNumberReport) checkRangeAndStatus(tN TrackingNumber) {
+	mutatedList = *list.List{}
+	for t := range obj.trackingNumberList {
+		if(tN.isDeleted()) {
+			break
+		}
+		if(!t.isDeleted()) {
+			mutatedList.addAll(tN.compare(t))
+		}
+	}
+
+	if(mutatedList != null) {
+		for t:= range mutatedList {
+				obj.checkRangeAndStatus(t)
+		}
+		obj.trackingNumberList.removeAll(mutatedList)
+		obj.trackingNumberList.addAll(mutatedList)
+	}
+	if(!tN.isDeleted()&& !obj.trackingNumberList.contains(tN)){
+		obj.trackingNumberList.add(tN)
+	}
 }
